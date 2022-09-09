ea665935b612472d85602672f68957726196321a
byeori <fkskqufgk@gmail.com>
2022-09-09 16:59:00 +0900

    message alert and redirect

---
+++ Board/src/main/java/com/board/study/common/dto/MessageDTO.java
@@ -0,0 +1,18 @@
+package com.board.study.common.dto;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageDTO {
+	
+	private String message;				//사용자에게 전달할 메시지
+	private String redirectUri;			//리다이렉트할 URI
+	private RequestMethod method;		//HTTP 요청 메소드
+	private Map<String, Object> data;	//화면으로 전달할 데이터(파라미터)
+}
--- Board/src/main/java/com/board/study/domain/post/PostController.java
+++ Board/src/main/java/com/board/study/domain/post/PostController.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.board.study.common.dto.MessageDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,16 +31,18 @@ public class PostController {
 	
 	//신규 게시글 생성
 	@PostMapping("/post/save.do")
-	public String savePost(final PostRequest params) {
+	public String savePost(final PostRequest params, Model model) {
 		postService.savePost(params);
-		return "redirect:/post/list.do";
+		MessageDTO message = new MessageDTO("게시글 생성이 완료되었습니다", "/post/list.do", RequestMethod.GET, null);
+		return showMessageAndRedirect(message, model);
 	}
 	
 	//게시글 업데이트
 	@PostMapping("/post/update.do")
-	public String updatePost(final PostRequest params) {
+	public String updatePost(final PostRequest params, Model model) {
 		postService.updatePost(params);
-		return "redirect:/post/list.do";
+		MessageDTO message = new MessageDTO("게시글 수정이 완료되었습니다", "/post/list.do", RequestMethod.GET, null);
+		return showMessageAndRedirect(message, model);
 	}
 	
 	//게시글 리스트
@@ -58,8 +63,15 @@ public class PostController {
 	
 	//게시글 삭제
 	@PostMapping("/post/delete.do")
-	public String deletePost(@RequestParam final Long id) {
+	public String deletePost(@RequestParam final Long id, Model model) {
 		postService.deletePost(id);
-		return "redirect:/post/list.do";
+		MessageDTO message = new MessageDTO("게시글 삭제가 완료되었습니다", "/post/list.do", RequestMethod.GET, null);
+		return showMessageAndRedirect(message, model);
+	}
+	
+	//사용자에게 메세지를 전달하고 페이지를 리다이렉트
+	private String showMessageAndRedirect(final MessageDTO params, Model model) {
+		model.addAttribute("params", params);
+		return "common/messageRedirect";
 	}
 }
